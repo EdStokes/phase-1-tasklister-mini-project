1bf3f618bf4937f394684e3f3d5c322a59250162
Ed Stokes <edstokes96@gmail.com>
2023-04-16 13:10:31 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    let form = document.querySelector("form")
+    form.addEventListener(`submit`, (e) => {
+    e.preventDefault()
+    buildToDo(e.target[`new-task-description`].value)
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+    let ul = document.createElement(`ul`)
+    let btn = document.createElement(`button`)
+    btn.addEventListener(`click`, handleDelete)
+    btn.textContent = ` x`
+    ul.textContent = `${todo}`
+    ul.appendChild(btn);
+    console.log(ul)
+    document.querySelector(`#tasks`).appendChild(ul)
+    ul.appendChild(btn)
+}
+
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
